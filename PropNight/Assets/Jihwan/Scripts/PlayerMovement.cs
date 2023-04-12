e2037a83c6dd84c5039929ea0035852ea1f9ab86
ryuji234 <jihwanryu7@gmail.com>
2023-04-12 12:50:02 +0900

    [ryu]fix merge

--- PropNight/Assets/Jihwan/Scripts/PlayerMovement.cs
+++ PropNight/Assets/Jihwan/Scripts/PlayerMovement.cs
@@ -23,9 +23,6 @@ public class PlayerMovement : MonoBehaviour
         Jump();
     }
 
-<<<<<<< HEAD
-        if (IsGrounded && velocity.y < 0)
-=======
     private void Move()
     {
         Vector3 moveDistance = _playerInput.MoveX*transform.right * Time.deltaTime + _playerInput.MoveZ*transform.forward * Time.deltaTime;
@@ -35,36 +32,11 @@ public class PlayerMovement : MonoBehaviour
     private void Jump()
     {
         if(Input.GetKeyDown(KeyCode.Space)) 
->>>>>>> a0327af4c4911765f703b183040c3049136c4c55
         {
             if(Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, 2))
             {
                 _playerRigidBody.AddForce(transform.up * jumpForce);
             }
         }
-<<<<<<< HEAD
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            controller.Move(move * speed * 2 * Time.deltaTime);
-        }
-        else
-        {
-            controller.Move(move * speed * Time.deltaTime);
-        }
-
-        if (Input.GetButtonDown("Jump") && IsGrounded)
-        {
-            velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
-=======
->>>>>>> a0327af4c4911765f703b183040c3049136c4c55
     }
 }

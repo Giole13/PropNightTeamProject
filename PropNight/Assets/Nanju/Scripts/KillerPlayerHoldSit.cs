0770806ea0fc96250255de6a937aa9194116f99f
eastsaul <freedman0829@naver.com>
2023-04-20 11:39:47 +0900

    3인칭 카메라 완료, 플레이어 들고 놨을때 하는중

--- PropNight/Assets/Nanju/Scripts/KillerPlayerHoldSit.cs
+++ PropNight/Assets/Nanju/Scripts/KillerPlayerHoldSit.cs
@@ -11,7 +11,10 @@ public class KillerPlayerHoldSit : MonoBehaviour
     // 플레이어 알기
     public GameObject Player;
     // 카메라 알기
-    public GameObject Camera;
+    // 1인칭 카메라
+    public GameObject FirstCamera;
+    // 3인칭 카메라
+    public GameObject ThirdCamera;
 
     [SerializeField]
     private KillerState _killerState = KillerState.IDLE;
@@ -32,7 +35,7 @@ public class KillerPlayerHoldSit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // MouseRightButton();
+        MouseRightButton();
         RightClick();
     }
 
@@ -45,7 +48,7 @@ public class KillerPlayerHoldSit : MonoBehaviour
         if (Input.GetMouseButtonDown(1))
         {
 
-            if (LookCamera.Obj.tag == "Player" /*&& LookCamera.ObjDistance < 3f*/)
+            if (LookCamera.Obj.tag == "Player" && LookCamera.ObjDistance < 3f)
             {
 
                 // 플레이어 스크립트 가져오기
@@ -63,8 +66,9 @@ public class KillerPlayerHoldSit : MonoBehaviour
                     // 플레이어 위치값 변경하기(들기)
                     Player.transform.position = HoldPlayerPosition.position;
                     // 카메라 3인칭 되게 하기
-                    Camera.transform.SetParent(null);
-
+                    ThirdCamera.SetActive(true);
+                    // 1인칭 카메라 끄기
+                    FirstCamera.SetActive(false);
                 }
             }
             // 플레이어 최면의자에 앉히기
@@ -74,9 +78,27 @@ public class KillerPlayerHoldSit : MonoBehaviour
                 LookCamera.Obj.GetComponent<IInteraction>().OnInteraction(Player);
                 // 플레이어 오브젝트 살인마 자식으로 빼기
                 Player.transform.SetParent(null);
+
+                // 1인칭 카메라 켜기
+                ThirdCamera.SetActive(false);
+                // 3인칭 카메라 끄기
+                FirstCamera.SetActive(true);
+            }
+            // 마우스 오른쪽 2번 클릭시 플레이어 원래 위치로 가기
+            else if (_killerState == KillerState.PLAYERHOLD)
+            {
+                // 플레이어 오브젝트 살인마 자식으로 빼기
+                Player.transform.SetParent(null);
+                Debug.Log("자식으로 갔니?");
+
+                // 1인칭 카메라 켜기
+                ThirdCamera.SetActive(false);
+                // 3인칭 카메라 끄기
+                FirstCamera.SetActive(true);
             }
 
         }
+
     }
     // 마우스 오른쪽 클릭시 
     public void MouseRightButton()
@@ -85,7 +107,7 @@ public class KillerPlayerHoldSit : MonoBehaviour
         {
             // 플레이어 들고 있는 시간 코루틴 실행
             StartCoroutine(HoldTime());
-            // 마우스 오른쪽 2번 클릭시 플레이어 원래 위치로 가기(나중에)
+
         }
     }
 

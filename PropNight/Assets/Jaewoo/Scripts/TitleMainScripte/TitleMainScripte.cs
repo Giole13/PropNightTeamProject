afa85ab68fbaa16908a38142f0351ad3ea7b1b33
DESKTOP-ILDHSHN\KGA_11 <wodn625@naver.com>
2023-04-21 14:28:06 +0900

    [Jaewoo] Title ui Finish

--- PropNight/Assets/Jaewoo/Scripts/TitleMainScripte/TitleMainScripte.cs
+++ PropNight/Assets/Jaewoo/Scripts/TitleMainScripte/TitleMainScripte.cs
@@ -6,11 +6,25 @@ using TMPro;
 
 public class TitleMainScripte : MonoBehaviour
 {
+    //=================GameObject===================
+    //타이틀&메인의 최상위 부모
     public GameObject mainCanvasObject = default;
+    //메인의 최상위 부모
     public GameObject mainObject = default;
+    //타이틀 최상위 부모
     public GameObject titleObject = default;
+    //Caution CanvasGroup Object
+    public GameObject cautionGroup = default;
+    //title CanvasGroup Object
+    public GameObject titleGroup = default;
+
+    //=================TMP_Text===================
+    //타이틀에서 텍스트 효과의 TMP_Text
     public TMP_Text titleText = default;
 
+    //=================정수 상수===================
+    //주의사항 페이드아웃
+    private float fadeOutCautionTime = 0f;
     //타이틀씬 anykey용 페이드인
     private float fadeInEffectTime = 0f;
     //타이틀씬 anykey용 페이드아웃
@@ -20,20 +34,23 @@ public class TitleMainScripte : MonoBehaviour
     private float fadeOutAllTitle = 0f;
     //메인씬 text전체용 페이드 인
     private float fadeInAllMain = 0f;
+
+    //=================Bool===================
     //title 씬에서 키 입력 받았는지
     public bool isAnyKeyDown = false;
     //title 씬에서 키 입력 받고 ui의 역할이 끝났는지
     public bool isEndTitle = false;
     //title 씬에서 fadeout 이 끝났는지
     public bool isTitleFadeOut = false;
+    //Caution 인지 아닌지
+    public bool isCautionEnd = false;
     void Start()
     {
         mainObject.GetComponent<CanvasGroup>().alpha = 0f;
-
+        titleGroup.GetComponent<CanvasGroup>().alpha = 0f;
     }
     void Update()
     {
-
         //키입력 없을때
         if (!Input.anyKeyDown)
         {
@@ -46,35 +63,40 @@ public class TitleMainScripte : MonoBehaviour
             isAnyKeyDown = true;
             isEndTitle = true;
         }
-
     }   //Update()
 
     #region 타이틀 텍스트 효과 함수
-
     public void TitleAnyKeyText()
     {
         if (!isAnyKeyDown)
         {
-            if (fadeInEffectTime < 1.5f)
+            if (!isCautionEnd)
             {
-                titleText.alpha = 0f;
-                titleText.text = "아무거나 입력하시오";
-                titleText.alpha = fadeInEffectTime / 1.5f;
+                StartCoroutine(Delay());
             }
             else
             {
-                fadeOutEffectTime = 2.5f;
+                titleText.alpha = 0f;
+                if (fadeInEffectTime < 1f)
+                {
+                    titleText.text = "아무거나 입력하시오";
+                    titleText.alpha = fadeInEffectTime / 1f;
+                }
+                else
+                {
+                    fadeOutEffectTime = 2f;
+                }
+                if (fadeInEffectTime < fadeOutEffectTime)
+                {
+                    titleText.alpha = 1f - fadeInEffectTime / fadeOutEffectTime;
+                }
+                if (2f < fadeInEffectTime)
+                {
+                    fadeInEffectTime = 0f;
+                    fadeOutEffectTime = 0f;
+                }
+                fadeInEffectTime += Time.deltaTime;
             }
-            if (fadeInEffectTime < fadeOutEffectTime)
-            {
-                titleText.alpha = 1f - fadeInEffectTime / fadeOutEffectTime;
-            }
-            if (2.5f < fadeInEffectTime)
-            {
-                fadeInEffectTime = 0f;
-                fadeOutEffectTime = 0f;
-            }
-            fadeInEffectTime += Time.deltaTime;
         }
         else
         {
@@ -82,9 +104,12 @@ public class TitleMainScripte : MonoBehaviour
             {
                 TitleAlpha();
             }
-            else { };
+            else
+            {
+
+            }
         }
-    }
+    }   //TitleAnyKeyText()
 
     //타이틀씬 ui Object 알파값 조절 함수
     public void TitleAlpha()
@@ -101,15 +126,13 @@ public class TitleMainScripte : MonoBehaviour
         }
         if (!isTitleFadeOut && fadeInAllMain < 1f)
         {
-            StartCoroutine(OneFrame());
-            TextFadeInAlpha(fadeInAllMain / 1f);
+            StartCoroutine(OneFrame(1f));
         }
         if (1f < fadeInAllMain)
         {
             isEndTitle = true;
         }
         fadeOutAllTitle += Time.deltaTime;
-        fadeInAllMain += Time.deltaTime;
 
     }   //TitleAlpha()
 
@@ -126,8 +149,31 @@ public class TitleMainScripte : MonoBehaviour
     }   //TextFadeInAlpha
     #endregion
 
-    IEnumerator OneFrame()
+    #region 코루틴
+    IEnumerator CautionTitle()
     {
-        yield return new WaitForSeconds(1f);
-    }
+        yield return new WaitForSeconds(0f);
+        TitleAnyKeyText();
+        titleGroup.GetComponent<CanvasGroup>().alpha = 1f;
+    }   //CautionTitle()
+    IEnumerator OneFrame(float frame)
+    {
+        yield return new WaitForSeconds(frame);
+        fadeInAllMain += Time.deltaTime;
+        TextFadeInAlpha(fadeInAllMain / 1f);
+    }   //OneFrame()
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(5f);
+        if (fadeOutCautionTime < 1f)
+        {
+            cautionGroup.GetComponent<CanvasGroup>().alpha = 1 - fadeOutCautionTime / 1f;
+            cautionGroup.GetComponent<CanvasGroup>().alpha = 0f;
+            StartCoroutine(CautionTitle());
+            isCautionEnd = true;
+        }
+        fadeOutCautionTime += Time.deltaTime;
+
+    }  // Delay()
+    #endregion
 }

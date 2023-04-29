cf8e7a51a8d76a4a77c15ccf7bd734c5ed8789cb
Parkjaewoo39 <wodn625@naver.com>
2023-04-29 20:57:58 +0900

    [Jaewoo] PropMachine Fix Bar

--- PropNight/Assets/Jaewoo/Scripts/PropFixBar.cs
+++ PropNight/Assets/Jaewoo/Scripts/PropFixBar.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class PropFixBar : MonoBehaviour
 {
-    public GameObject propMachineFixedCheck = default;
+
+    public GameObject propMachineFixedCheck;
 
     [Header("이미지")]
     //막대기 
@@ -28,58 +30,71 @@ public class PropFixBar : MonoBehaviour
     public float maxSuccess = default;
     public float minPerfect = default;
     public float maxPerfect = default;
-    public int successBarAngle = default;
-    public int perfectBarAngle = default;
+    public float successBarAngle = default;
+    public float perfectBarAngle = default;
+    public float textFadeOut = default;
+    public float textFadeIn = default;
 
     void Start()
     {
         propMachineFixedCheck.SetActive(false);
-        //밸류 초기화
-        timingValue = 0f;
+
     }
 
-    /// <summary>
-    /// FillAmount 조절시 stick이 움직ㅇ
-    /// </summary>
-    /// <param name="timingValue_"></param>
     public void SkillCheck()
     {
         propMachineFixedCheck.SetActive(true);
+        circleBar.fillAmount = 0f;
         StartCoroutine(SkillCheckRoutine());
+
+
+        // stick.localEulerAngles = new Vector3(0, 0, 0);
+        // successStick.localEulerAngles = new Vector3(0, 0, 0);
+        // perfectStick.localEulerAngles = new Vector3(0, 0, 0);
     }
+
     IEnumerator SkillCheckRoutine()
     {
+        timingValue_ = 0;
+        barAmount = 0;
+
+        //성공 대성공 앵글 초기화
         successBarAngle = Random.Range(-20, -80);
-        perfectBarAngle = (successBarAngle);
+
+        perfectBarAngle = (successBarAngle - 36f);
+
         successStick.localEulerAngles = new Vector3(0, 0, successBarAngle);
         perfectStick.localEulerAngles = new Vector3(0, 0, perfectBarAngle);
-        // perfectStick.localEulerAngles = new Vector3(0, 0, perfectBarAngle);
 
         while (circleBar.fillAmount < 0.4f && !Input.GetKeyDown(KeyCode.Space))
         {
             timingValue_ += Time.deltaTime * 70;
             barAmount = (timingValue_ * 0.01f) * 0.5f;
-            stickAngle = barAmount * 360;
             circleBar.fillAmount = barAmount;
+            stickAngle = barAmount * 360;
             stick.localEulerAngles = new Vector3(0, 0, -stickAngle);
             yield return null;
         }
 
-        if (Mathf.Abs((successBarAngle)) <= stickAngle &&
-            Mathf.Abs((successBarAngle)) + 54 >= stickAngle)
+        if (stickAngle < Mathf.Abs(successBarAngle) || Mathf.Abs(perfectBarAngle - 18) < stickAngle)
         {
-            Debug.Log("난 성공했다.");
+            Debug.Log("ㅂㅈㄷ");
+            //실패 연결
         }
-        if (Mathf.Abs((perfectBarAngle)) <= stickAngle &&
-            Mathf.Abs((perfectBarAngle)) + 18 >= stickAngle)
+
+        if (Mathf.Abs(successBarAngle) <= stickAngle && stickAngle < Mathf.Abs(successBarAngle - 36))
         {
-            Debug.Log("난 대성공했다.");
+            Debug.Log("성공");
+            //성공 연결
+
         }
+        if (Mathf.Abs(perfectBarAngle) <= stickAngle && stickAngle <= Mathf.Abs(perfectBarAngle - 18))
+        {
+            Debug.Log("대ㅔ성공");
+            //대성공 연결
+        }
+
         propMachineFixedCheck.SetActive(false);
-        timingValue_ = 0f;
-        barAmount = 0f;
-        stickAngle = 0f;
-        circleBar.fillAmount = 0f;
-        stick.localEulerAngles = new Vector3(0, 0, -stickAngle);
     }
+
 }
